bed82dbf2e05c573856a6b5d980c28cbee15d318
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-05-01 10:24:28 +0200

    refine the initialization of the `Migrate` class

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, directory="src/migrations")
 
 
 class Category(db.Model):
