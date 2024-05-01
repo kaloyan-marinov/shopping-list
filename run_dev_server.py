18a1179ed87705d279f0c02673dcf68488688eb1
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-05-01 11:26:03 +0200

    modify `run_dev_server.py` to set `use_reloader=True`

--- run_dev_server.py
+++ run_dev_server.py
@@ -2,4 +2,4 @@ from src import app
 
 
 if __name__ == "__main__":
-    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
+    app.run(use_debugger=False, use_reloader=True, passthrough_errors=True)
